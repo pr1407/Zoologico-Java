c25c085327d739d8f733cc6ccfd3bff7c9f68829
Pedro Ruiz <otaku14.pr@gmail.com>
2020-12-01 08:16:10 -0500

    Funcion main estable

--- principal/zoologico.java
+++ principal/zoologico.java
@@ -7,6 +7,8 @@ import hijas.Tigre;
 public class zoologico {
     public static void main (String[] args ){
          infoVicunia();
+         infoTigre();
+         infoGallina();
     }
     
     public static void infoVicunia(){
@@ -18,5 +20,26 @@ public class zoologico {
         System.out.print(vicunia.comer());
         System.out.print(vicunia.hacerRuido());
         System.out.print("-------------------------------------- \n");
+    }
+    
+    public static void infoTigre(){
+        Animal tigre = new Tigre("Scar" ,10);
+        System.out.print("Nombre Vicunia: "+ tigre.getNombre()+"\n");
+        System.out.print("Edad: " + tigre.getEdad()+" anios\n");
+        System.out.print("Color: "+ tigre.getColor()+"\n");
+        System.out.print("Peso: "+ tigre.getPeso()+" kg \n");
+        System.out.print(tigre.comer());
+        System.out.print(tigre.hacerRuido());
+        System.out.print("-------------------------------------- \n");        
+    }
+    public static void infoGallina(){
+        Animal gallina = new Gallina("Marron" , 6);
+        System.out.print("Nombre Vicunia: "+ gallina.getNombre()+"\n");
+        System.out.print("Edad: " + gallina.getEdad()+" anios\n");
+        System.out.print("Color: "+ gallina.getColor()+"\n");
+        System.out.print("Peso: "+ gallina.getPeso()+" kg \n");
+        System.out.print(gallina.comer());
+        System.out.print(gallina.hacerRuido());
+        System.out.print("-------------------------------------- \n");        
     }
 }
